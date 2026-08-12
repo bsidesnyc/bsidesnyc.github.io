96d5148919a577b19272df4e6959e8b20f56b53f
august huber <gus@pbx.org>
2026-08-12 19:57:00 -0400

    Enable speakers page, note that CFP is complete (#259)
    * Add 2026 Tech CFP bios
    
    * enable speakers page, optimize speaker image size and prefetch
    
    * clean up cards to match styles

--- js/speakers.js
+++ js/speakers.js
@@ -25,7 +25,7 @@ function addSpeakerToList(speaker, sessions, rooms, container) {
         }
 
         const wrapper = document.createElement("div");
-        wrapper.className = "effect-wrapper col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 text-end speaker-card";
+        wrapper.className = "effect-wrapper col-md-4 col-sm-6 col-12 text-end speaker-card";
 
         const effect = document.createElement("div");
         effect.className = "zoe-effect";
@@ -34,10 +34,13 @@ function addSpeakerToList(speaker, sessions, rooms, container) {
 
         const headshot = document.createElement("figure");
 
-        const bg = document.createElement("div");
+        const bg = document.createElement("img");
         bg.className = "card-bg";
+        bg.loading = "lazy";
+        bg.decoding = "async";
+        bg.alt = speaker.fullName || "";
         if (speaker.profilePicture) {
-            bg.style.backgroundImage = "url(" + speaker.profilePicture + ")";
+            bg.src = getSessionizeThumbnail(speaker.profilePicture, 400);
         }
         headshot.append(bg);
 
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,8 @@
+function getSessionizeThumbnail(url, size) {
+    if (!url) return url;
+    return url.replace(/-\d+o\d+o\d+-/, `-${size}o${size}o1-`);
+}
+
 function normalizeTrackName(trackName) {
     return trackName.replace(/\s/g, "").toLowerCase();;
 }
@@ -65,20 +70,26 @@ function getTrackNameAndRoomHtml(trackName, heading, closeModal) {
 }
 
 function getTrackNameAndRoom(trackName) {
+    if ( !trackName ) {
+        return "TBD";
+    }
     var elem = trackToRoomElements(trackName);
     return elem.tight;
 }
 
 function getStartEndString(session) {
+    if ( !session.startsAt || !session.endsAt ) {
+        return "TBD";
+    }
     var start = new Date(session.startsAt);
     var end = new Date(session.endsAt);
 
-    return start.getHours() + 
-        ":" + 
-        padMinutes(start) + 
-        " - " + 
-        end.getHours() + 
-        ":" + 
+    return start.getHours() +
+        ":" +
+        padMinutes(start) +
+        " - " +
+        end.getHours() +
+        ":" +
         padMinutes(end);
 }
 function padMinutes(time) {
@@ -128,7 +139,7 @@ function createBootstrapModal(id, labelId) {
 function getSessionLevel(session) {
     const categoryLevelId = 75411;
     if (!session || !session.categories) {
-        return "Not provided - Open to All";
+        return "TBD";
     }
     const category = session.categories.find(cat => cat.id === categoryLevelId);
     if (category && category.categoryItems) {
@@ -137,7 +148,7 @@ function getSessionLevel(session) {
             return levelItem.name;
         }
     }
-    return "Not provided - Open to All";
+    return "TBD";
 }
 
 function addSessionContentToModal(modalBody, session, trackName) {
@@ -178,23 +189,26 @@ function addSpeakerContentToModal(modalBody, speaker, baseUrl) {
     row.classList.add("row");
 
     const headshot = document.createElement("div");
-    headshot.classList.add("col-md-2", "col-sm-2");
+    headshot.classList.add("col-md-2", "col-12");
 
-    const speakerHeadshot = document.createElement("div");
+    const speakerHeadshot = document.createElement("img");
     speakerHeadshot.id = "speakerProfileUrl-" + speaker.id;
     speakerHeadshot.classList.add("flow-img", "img-circle", "people-img");
+    speakerHeadshot.loading = "lazy";
+    speakerHeadshot.decoding = "async";
+    speakerHeadshot.alt = speaker.fullName || speaker.name || "";
     if (speaker.profilePicture) {
-        speakerHeadshot.style.backgroundImage = `url(${speaker.profilePicture})`;
+        speakerHeadshot.src = speaker.profilePicture;
     }
     headshot.append(speakerHeadshot);
     row.append(headshot);
 
     const details = document.createElement("div");
-    details.classList.add("col-md-10", "col-sm-10", "details");
+    details.classList.add("col-md-10", "col-12", "details");
 
     const name = document.createElement("p");
     name.classList.add("name", "mb-2");
-    name.innerText = speaker.fullName;
+    name.innerText = speaker.fullName || speaker.name || "";
     details.append(name);
 
     const tagline = document.createElement("p");
